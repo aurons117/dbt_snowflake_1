3e5a38420090e771eb350920aac52f6e96a5d4e2
Ivan Sandin <icarbajals0600@gmail.com>
2025-01-19 06:56:49 +0000

    Adding testings

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,10 +7,7 @@ select
     paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
-    {{ cents_to_dollars('amount') }} as amount,
+    amount / 100 as amount,
     created as created_at
 
--- from {{ source('stripe','payment')}}
 from raw.stripe.payment
-
-
