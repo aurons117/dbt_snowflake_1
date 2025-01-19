dee2416f29a7b4a8134426d2c25ef958876528b0
Ivan Sandin <icarbajals0600@gmail.com>
2025-01-19 02:25:29 +0000

    adding structure to project

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,16 @@
+
+-- stg_payments
+
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount') }} as amount,
+    created as created_at
+
+-- from {{ source('stripe','payment')}}
+from raw.stripe.payment
+
+
